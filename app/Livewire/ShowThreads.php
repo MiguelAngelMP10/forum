229fd01fc071e6cb066bfe168b315a346d765db0
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-08-29 22:02:20 -0600

    Tabla categorías

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
@@ -11,6 +12,9 @@ class ShowThreads extends Component
 {
     public function render(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
-        return view('livewire.show-threads');
+        $categories = Category::get();
+        return view('livewire.show-threads', [
+            'categories' => $categories
+        ]);
     }
 }

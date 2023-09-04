0772eb3c893ebebd2b04091e7dc6c70d91fc26b2
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-09-04 12:33:46 -0600

    Segundo componente livewire (pregunta)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\ShowThread;
 use App\Livewire\ShowThreads;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', ShowThreads::class)
     ->middleware('auth')
     ->name('dashboard');
+
+Route::get('/thread/{thread}', ShowThread::class)
+    ->middleware('auth')
+    ->name('thread');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
